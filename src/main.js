75276f6da85dda8232fc3463ae8386b9e4a87714
Alena Ismagilova <ismagilovaalena90@yandex.ru>
2023-10-18 00:04:13 +0400

    fix:env var

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import { createAuth0 } from '@auth0/auth0-vue'
+import { domain, clientId } from '../auth_config.json'
 
 const app = createApp(App)
 
@@ -10,10 +11,8 @@ app.use(router)
 
 app.use(
   createAuth0({
-    // domain: domain,
-    // clientId: clientId,
-    domain: import.meta.env.DOMAIN || '',
-    clientId: import.meta.env.CLIENT_ID || '',
+    domain: domain,
+    clientId: clientId,
     authorizationParams: {
       redirect_uri: 'https://auth-vue-5d5d.vercel.app/'
     }
