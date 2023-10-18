baa8c23216b97721da84d8e06acf1653ce584a13
Alena Ismagilova <ismagilovaalena90@yandex.ru>
2023-10-18 13:41:28 +0400

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,19 @@ import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  define: {
-    VITE_DOMAIN: process.env.VITE_DOMAIN,
-    VITE_CLIENT_ID: process.env.VITE_CLIENT_ID
-  },
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    define: {
+      VITE_DOMAIN: env.VITE_DOMAIN,
+      VITE_CLIENT_ID: env.VITE_CLIENT_ID
+    },
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
     }
   }
 })
